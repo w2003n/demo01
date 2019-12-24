565925dbc4565cd21281786f53c086bbf56ac038
w2003n <w2737879820@163.com>
2019-12-24 14:01:44 +0800

    输出

---
+++ demo01/src/main/java/com/git/demo01/Demo01Application.java
@@ -0,0 +1,14 @@
+package com.git.demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01Application {
+
+    public static void main(String[] args) {
+        System.out.println("dfdfdfdfdfdfdf");
+        SpringApplication.run(Demo01Application.class, args);
+    }
+
+}
---
+++ demo01/src/test/java/com/git/demo01/Demo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
